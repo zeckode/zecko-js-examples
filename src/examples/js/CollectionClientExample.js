eb2e7b05ebf8f4a3747943e0db3c04b033a86ff8
akartik80 <akartik80@gmail.com>
2022-05-29 23:59:43 +0530

    minor changes

--- src/examples/js/CollectionClientExample.js
+++ src/examples/js/CollectionClientExample.js
@@ -12,26 +12,12 @@ class CollectionClientExample {
   };
 }
 
-/**
- * ! --- A trailing exclamation mark is used to denote a field that uses a Non‐Null type
- */
-
 const collectionClientExample = new CollectionClientExample();
 
-/**
- * PRODUCT_ID (String!) --- product_id of the product that you want to get
- */
 console.log(
   JSON.stringify(await collectionClientExample.getbyId('YOUR_COLLECTION_ID'))
 );
-
 console.log('\n');
-
-/**
- * AFTER (String) --- 'after' perameter is used to achieve pagination of the collection page
- *                    -- if you want to get the first collection page, you can omit this parameter
- *                    -- if you want to get the next collection page, you can pass the 'endCursor' value in this parameter
- *                       - 'endCursor' value is returned in the 'pageInfo' object of 'collections' object
- *                       -  you can pass any 'curser' value of an object to get next collections
- */
-console.log(JSON.stringify(await collectionClientExample.getAll('YOUR_CURSOR')));
+console.log(
+  JSON.stringify(await collectionClientExample.getAll('COLLECTION_CURSOR'))
+);
--- src/examples/js/ProductClientExample.js
+++ src/examples/js/ProductClientExample.js
@@ -12,44 +12,14 @@ class ProductClientExample {
   };
 }
 
-/**
- * ! --- A trailing exclamation mark is used to denote a field that uses a Non‐Null type
- */
-
 const productClientExample = new ProductClientExample();
 
-/**
- * COLLECTION_ID (String!) --- 'collection_id' of the collection that you want to get all products from
- *
- * AFTER (String)          --- 'after' perameter is used to achieve pagination of the results
- *                             -- if you want to get the first page, you can omit this parameter
- *                             -- if you want to get the next page, you can pass the 'endCursor' value in this parameter
- *                                - 'endCursor' value is returned in the 'pageInfo' object of 'product' object
- *                                -  you can pass any 'curser' value of an object to get next objects
- */
 console.log(
   JSON.stringify(
     await productClientExample.getAllByCollectionId('YOUR_COLLECTION_ID')
   )
 );
-
 console.log('\n');
-
-/**
- * ID (String!)             --- 'product_id' of the product that you want to get
- *
- * IMAGESAFTER (String)     --- 'imagesAfter' parameter is used to achieve the pagination of product images
- *                              -- If you want to get the first page of images, pass null as 'imagesAfter'
- *                              -- If you want to get the next page of images, pass the 'endCursor' of the current page as 'imagesAfter'
- *                                 - 'endCursor' value is returned in the 'pageInfo' object of 'images' object
- *                                 - you can pass any 'cursor' value of an object to get next images
- *
- * VARIANTSAFTER (String)   --- 'variantsAfter' parameter is used to achieve the pagination of product variants
- *                              -- If you want to get the first page of variants, pass null as 'variantsAfter'
- *                              -- If you want to get the next page of variants, pass the 'endCursor' of the current page as 'variantsAfter'
- *                                 - 'endCursor' value is returned in the 'pageInfo' object of 'variants' object
- *                                 - you can pass any 'cursor' value of an object to get next variants
- */
 console.log(
   JSON.stringify(
     await productClientExample.getById(
