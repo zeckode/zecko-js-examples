c5eeca61d37a42c712eb7da7e8b9b53e99fec5b1
Prince Patoliya <princepatoliya852@gmail.com>
2022-05-29 17:38:04 +0530

    - add docs for product and collection client

--- src/examples/js/CollectionClientExample.js
+++ src/examples/js/CollectionClientExample.js
@@ -1,20 +1,37 @@
 import { Zecko } from 'zecko-js';
 
 class CollectionClientExample {
-  get = (id) => {
+  getbyId = (id) => {
     const zecko = new Zecko({ accessToken: 'YOUR_ACCESS_TOKEN' });
-    return zecko.collectionClient.get(id);
+    return zecko.collectionClient.getbyId(id);
   };
 
-  getAll = () => {
+  getAll = (after = null) => {
     const zecko = new Zecko({ accessToken: 'YOUR_ACCESS_TOKEN' });
-    return zecko.collectionClient.getAll();
+    return zecko.collectionClient.getAll(after);
   };
 }
 
+/**
+ * ! --- A trailing exclamation mark is used to denote a field that uses a Non‐Null type
+ */
+
 const collectionClientExample = new CollectionClientExample();
+
+/**
+ * PRODUCT_ID (String!) --- product_id of the product that you want to get
+ */
 console.log(
-  JSON.stringify(await collectionClientExample.get('YOUR_COLLECTION_ID'))
+  JSON.stringify(await collectionClientExample.getbyId('YOUR_COLLECTION_ID'))
 );
+
 console.log('\n');
-console.log(JSON.stringify(await collectionClientExample.getAll()));
+
+/**
+ * AFTER (String) --- 'after' perameter is used to achieve pagination of the collection page
+ *                    -- if you want to get the first collection page, you can omit this parameter
+ *                    -- if you want to get the next collection page, you can pass the 'endCursor' value in this parameter
+ *                       - 'endCursor' value is returned in the 'pageInfo' object of 'collections' object
+ *                       -  you can pass any 'curser' value of an object to get next collections
+ */
+console.log(JSON.stringify(await collectionClientExample.getAll('YOUR_CURSOR')));
--- src/examples/js/ProductClientExample.js
+++ src/examples/js/ProductClientExample.js
@@ -6,46 +6,30 @@ class ProductClientExample {
     return zecko.productClient.getAllByCollectionId(collectionId, after);
   };
 
-  getByProductId = (id, imagesAfter = null, variantsAfter= null) => {
+  getById = (id, imagesAfter = null, variantsAfter = null) => {
     const zecko = new Zecko({ accessToken: 'YOUR_ACCESS_TOKEN' });
-    return zecko.productClient.getByProductId(id, imagesAfter, variantsAfter);
+    return zecko.productClient.getById(id, imagesAfter, variantsAfter);
   };
 }
 
 /**
  * ! --- A trailing exclamation mark is used to denote a field that uses a Non‐Null type
- * 
- */ 
+ */
 
 const productClientExample = new ProductClientExample();
 
 /**
  * COLLECTION_ID (String!) --- 'collection_id' of the collection that you want to get all products from
- * 
- */
-console.log(
-  JSON.stringify(
-    await productClientExample.getAllByCollectionId('COLLECTION_ID')
-  )
-);
-
-console.log('\n');
-
-/**
- * COLLECTION_ID (String!) --- 'collection_id' of the collection that you want to get all products from
- * 
+ *
  * AFTER (String)          --- 'after' perameter is used to achieve pagination of the results
  *                             -- if you want to get the first page, you can omit this parameter
  *                             -- if you want to get the next page, you can pass the 'endCursor' value in this parameter
- *                                - 'endCursor' value is returned in the 'pageInfo' object of the previous response
- * 
+ *                                - 'endCursor' value is returned in the 'pageInfo' object of 'product' object
+ *                                -  you can pass any 'curser' value of an object to get next objects
  */
 console.log(
   JSON.stringify(
-    await productClientExample.getAllByCollectionId(
-      'COLLECTION_ID',
-      'AFTER'
-    )
+    await productClientExample.getAllByCollectionId('YOUR_COLLECTION_ID')
   )
 );
 
@@ -53,26 +37,25 @@ console.log('\n');
 
 /**
  * ID (String!)             --- 'product_id' of the product that you want to get
- * 
+ *
  * IMAGESAFTER (String)     --- 'imagesAfter' parameter is used to achieve the pagination of product images
  *                              -- If you want to get the first page of images, pass null as 'imagesAfter'
  *                              -- If you want to get the next page of images, pass the 'endCursor' of the current page as 'imagesAfter'
- *                                 - 'endCursor' value is returned in the 'pageInfo' object of images object of the previous response
- *                                 - you can pass the 'cursor' value of image node as well, then you got the next images according that
- * 
+ *                                 - 'endCursor' value is returned in the 'pageInfo' object of 'images' object
+ *                                 - you can pass any 'cursor' value of an object to get next images
+ *
  * VARIANTSAFTER (String)   --- 'variantsAfter' parameter is used to achieve the pagination of product variants
  *                              -- If you want to get the first page of variants, pass null as 'variantsAfter'
- *                              -- If you want to get the next page of variants, pass the 'endCursor' of the current page as 'variantsAfter'  
- *                                 - 'endCursor' value is returned in the 'pageInfo' object of variants object of the previous response
- *                                 - you can pass the 'cursor' value of variant node as well, then you got the next variants according that
- * 
+ *                              -- If you want to get the next page of variants, pass the 'endCursor' of the current page as 'variantsAfter'
+ *                                 - 'endCursor' value is returned in the 'pageInfo' object of 'variants' object
+ *                                 - you can pass any 'cursor' value of an object to get next variants
  */
 console.log(
   JSON.stringify(
-    await productClientExample.getByProductId(
-      'ID',
-      'IMAGESAFTER',
-      'VARIANTSAFTER'
+    await productClientExample.getById(
+      'YOUR_PRODUCT_ID',
+      'YOUR_IMAGE_CURSOR',
+      'YOUR_VARIANT_CURSOR'
     )
   )
 );
