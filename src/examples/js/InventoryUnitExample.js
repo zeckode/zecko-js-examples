1195daca1e115680f127da459786a34d6f483f03
princepatoliya <princepatoliya852@gmail.com>
2022-08-05 14:53:56 +0530

    - add inventory body to apis

--- src/examples/js/InventoryUnitExample.js
+++ src/examples/js/InventoryUnitExample.js
@@ -1,41 +1,63 @@
 import { Zecko } from 'zecko';
 
 class InventoryUnitExample {
-  cancelById = (inventoryUnitId) => {
+  cancelById = (inventoryUnitId, inventoryUnitActionRequest) => {
     const zecko = new Zecko({ accessToken: 'YOUR_ACCESS_TOKEN' });
-    return zecko.inventoryUnitClient.cancelById(inventoryUnitId);
+    return zecko.inventoryUnitClient.cancelById(
+      inventoryUnitId,
+      inventoryUnitActionRequest
+    );
   };
 
-  returnById = (inventoryUnitId) => {
+  returnById = (inventoryUnitId, inventoryUnitActionRequest) => {
     const zecko = new Zecko({ accessToken: 'YOUR_ACCESS_TOKEN' });
-    return zecko.inventoryUnitClient.returnById(inventoryUnitId);
+    return zecko.inventoryUnitClient.returnById(
+      inventoryUnitId,
+      inventoryUnitActionRequest
+    );
   };
 
-  exchangeById = (inventoryUnitId) => {
+  exchangeById = (inventoryUnitId, inventoryUnitActionRequest) => {
     const zecko = new Zecko({ accessToken: 'YOUR_ACCESS_TOKEN' });
-    return zecko.inventoryUnitClient.exchangeById(inventoryUnitId);
+    return zecko.inventoryUnitClient.exchangeById(
+      inventoryUnitId,
+      inventoryUnitActionRequest
+    );
   };
 
-  trackById = (inventoryUnitId) => {
+  trackById = (inventoryUnitId, inventoryUnitActionRequest) => {
     const zecko = new Zecko({ accessToken: 'YOUR_ACCESS_TOKEN' });
-    return zecko.inventoryUnitClient.trackById(inventoryUnitId);
+    return zecko.inventoryUnitClient.trackById(
+      inventoryUnitId,
+      inventoryUnitActionRequest
+    );
   };
 }
 
 const inventoryUnitClient = new InventoryUnitExample();
 
 console.log(
-  JSON.stringify(await inventoryUnitClient.cancelById('YOUR_INVENTORY_UNIT_ID'))
-);
-
-console.log(
   JSON.stringify(
-    await inventoryUnitClient.exchangeById('YOUR_INVENTORY_UNIT_ID')
+    await inventoryUnitClient.cancelById('YOUR_INVENTORY_UNIT_ID', {
+      cancelReason: 'YOUR_CANCEL_REASON',
+    })
   )
 );
 
 console.log(
-  JSON.stringify(await inventoryUnitClient.returnById('YOUR_INVENTORY_UNIT_ID'))
+  JSON.stringify(
+    await inventoryUnitClient.exchangeById('YOUR_INVENTORY_UNIT_ID', {
+      exchangeReason: 'YOUR_EXCHANGE_REASON',
+    })
+  )
+);
+
+console.log(
+  JSON.stringify(
+    await inventoryUnitClient.returnById('YOUR_INVENTORY_UNIT_ID', {
+      returnReason: 'YOUR_RETURN_REASON',
+    })
+  )
 );
 
 console.log(
